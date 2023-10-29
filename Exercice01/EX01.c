fb85bbc3f0a8531401ab1791f0fa4cbd157f452a
AIsamet <isamet2067@gmail.com>
2023-10-29 16:14:38 +0100

    Completed EX02.c and commented EX01.c

--- Exercice01/EX01.c
+++ Exercice01/EX01.c
@@ -1,16 +1,14 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<errno.h>
-#include<getopt.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <getopt.h>
 
 #define STDOUT 1
 #define STDERR 2
 
 #define MAX_PATH_LENGTH 4096
 #define BUFFER_SIZE 8192
-
 
 #define USAGE_SYNTAX "[OPTIONS] -i INPUT -o OUTPUT"
 #define USAGE_PARAMS "OPTIONS:\n\
@@ -27,11 +25,10 @@
  *
  * \return void
  */
-void print_usage(char* bin_name)
+void print_usage(char *bin_name)
 {
   dprintf(1, "USAGE: %s %s\n\n%s\n", bin_name, USAGE_SYNTAX, USAGE_PARAMS);
 }
-
 
 /**
  * Procedure checks if variable must be free
@@ -41,11 +38,11 @@ void print_usage(char* bin_name)
  * \see man 3 free
  * \return void
  */
-void free_if_needed(void* to_free)
+void free_if_needed(void *to_free)
 {
-  if (to_free != NULL) free(to_free);  
+  if (to_free != NULL)
+    free(to_free);
 }
-
 
 /**
  *
@@ -53,22 +50,21 @@ void free_if_needed(void* to_free)
  * \see man 3 perror
  * \return
  */
-char* dup_optarg_str()
+char *dup_optarg_str()
 {
-  char* str = NULL;
+  char *str = NULL;
 
   if (optarg != NULL)
   {
     str = strndup(optarg, MAX_PATH_LENGTH);
-    
+
     // Checking if ERRNO is set
-    if (str == NULL) 
+    if (str == NULL)
       perror(strerror(errno));
   }
 
   return str;
 }
-
 
 /**
  * Binary options declaration
@@ -77,39 +73,36 @@ char* dup_optarg_str()
  * \see man 3 getopt_long or getopt
  * \see struct option definition
  */
-static struct option binary_opts[] = 
-{
-  { "help",    no_argument,       0, 'h' },
-  { "verbose", no_argument,       0, 'v' },
-  { "input",   required_argument, 0, 'i' },
-  { "output",  required_argument, 0, 'o' },
-  { 0,         0,                 0,  0  } 
-};
+static struct option binary_opts[] =
+    {
+        {"help", no_argument, 0, 'h'},
+        {"verbose", no_argument, 0, 'v'},
+        {"input", required_argument, 0, 'i'},
+        {"output", required_argument, 0, 'o'},
+        {0, 0, 0, 0}};
 
 /**
  * Binary options string
  * (linked to optionn declaration)
  *
  * \see man 3 getopt_long or getopt
- */ 
-const char* binary_optstr = "hvi:o:";
-
-
+ */
+const char *binary_optstr = "hvi:o:";
 
 /**
  * Binary main loop
  *
- * \return 1 if it exit successfully 
+ * \return 1 if it exit successfully
  */
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   /**
    * Binary variables
    * (could be defined in a structure)
    */
   short int is_verbose_mode = 0;
-  char* bin_input_param = NULL;
-  char* bin_output_param = NULL;
+  char *bin_input_param = NULL;
+  char *bin_output_param = NULL;
 
   // Parsing options
   int opt = -1;
@@ -119,35 +112,33 @@ int main(int argc, char** argv)
   {
     switch (opt)
     {
-      case 'i':
-        //input param
-        if (optarg)
-        {
-          bin_input_param = dup_optarg_str();         
-        }
-        break;
-      case 'o':
-        //output param
-        if (optarg)
-        {
-          bin_output_param = dup_optarg_str();
-        }
-        break;
-      case 'v':
-        //verbose mode
-        is_verbose_mode = 1;
-        break;
-      case 'h':
-        print_usage(argv[0]);
-
-        free_if_needed(bin_input_param);
-        free_if_needed(bin_output_param);
- 
-        exit(EXIT_SUCCESS);
-      default :
-        break;
+    case 'i':
+      // input param
+      if (optarg)
+      {
+        bin_input_param = dup_optarg_str();
+      }
+      break;
+    case 'o':
+      // output param
+      if (optarg)
+      {
+        bin_output_param = dup_optarg_str();
+      }
+      break;
+    case 'v':
+      // verbose mode
+      is_verbose_mode = 1;
+      break;
+    case 'h':
+      print_usage(argv[0]);
+      free_if_needed(bin_input_param);
+      free_if_needed(bin_output_param);
+      exit(EXIT_SUCCESS);
+    default:
+      break;
     }
-  } 
+  }
 
   /**
    * Checking binary requirements
@@ -164,57 +155,60 @@ int main(int argc, char** argv)
     exit(EXIT_FAILURE);
   }
 
-
   // Printing params
-  dprintf(1, "** PARAMS **\n%-8s: %s\n%-8s: %s\n%-8s: %d\n", 
-          "input",   bin_input_param, 
-          "output",  bin_output_param, 
+  dprintf(1, "** PARAMS **\n%-8s: %s\n%-8s: %s\n%-8s: %d\n",
+          "input", bin_input_param,
+          "output", bin_output_param,
           "verbose", is_verbose_mode);
 
   // Business logic must be implemented at this point
   // V1.0
+  //
+  // Open the source file for reading
+  FILE *source_file = fopen(bin_input_param, "rb"); // Open the source file for reading in binary mode
+  if (source_file == NULL)
+  {
+    perror("Failed to open the source file for reading");
+    exit(EXIT_FAILURE);                             // Exit the program with an error status
+  }
 
-    // Open the source file for reading
-    FILE* source_file = fopen(bin_input_param, "rb");
-    if (source_file == NULL) {
-        perror("Failed to open the source file for reading");
-        exit(EXIT_FAILURE);
+  // Create or open the destination file for writing
+  FILE *dest_file = fopen(bin_output_param, "wb");  // Open the destination file for writing in binary mode
+  if (dest_file == NULL)
+  {
+    perror("Failed to open or create the destination file for writing");
+    fclose(source_file);                            // Close the source file to release resources
+    exit(EXIT_FAILURE);                             // Exit the program with an error status
+  }
+
+  char buffer[BUFFER_SIZE];                         // Allocate a buffer to store read data
+  size_t bytes_read;                                // Variable to store the number of bytes read
+
+  // Loop to read and write the file contents in chunks
+  while ((bytes_read = fread(buffer, 1, BUFFER_SIZE, source_file)) > 0)
+  {
+    // Write the read data to the destination file
+    if (fwrite(buffer, 1, bytes_read, dest_file) != bytes_read)
+    {
+      perror("Failed to write to destination file");
+      fclose(source_file);                          // Close the source file
+      fclose(dest_file);                            // Close the destination file
+      exit(EXIT_FAILURE);                           // Exit the program with a failure status
     }
 
-    // Create or open the destination file for writing
-    FILE* dest_file = fopen(bin_output_param, "wb");
-    if (dest_file == NULL) {
-        perror("Failed to open or create the destination file for writing");
-        fclose(source_file);  // Close the source file to release resources
-        exit(EXIT_FAILURE);   // Exit the program with a failure status
+    if (is_verbose_mode)
+    {
+      printf("Copied %zu bytes\n", bytes_read);     // Print the number of bytes copied if in verbose mode
     }
+  }
 
-    char buffer[BUFFER_SIZE];  // Allocate a buffer to store read data
-    size_t bytes_read;         // Variable to store the number of bytes read
-
-    // Loop to read and write the file contents in chunks
-    while ((bytes_read = fread(buffer, 1, BUFFER_SIZE, source_file)) > 0) {
-        // Write the read data to the destination file
-        if (fwrite(buffer, 1, bytes_read, dest_file) != bytes_read) {
-            perror("Failed to write to destination file");
-            fclose(source_file);  // Close the source file
-            fclose(dest_file);    // Close the destination file
-            exit(EXIT_FAILURE);   // Exit the program with a failure status
-        }
-
-        if (is_verbose_mode) {
-            printf("Copied %zu bytes\n", bytes_read);  // Print the number of bytes copied if in verbose mode
-        }
-    }
-
-    // Close the files
-    fclose(source_file);  // Close the source file
-    fclose(dest_file);    // Close the destination file
+  // Close the files
+  fclose(source_file);                              // Close the source file
+  fclose(dest_file);                                // Close the destination file
 
   // Freeing allocated data
-  free_if_needed(bin_input_param);
-  free_if_needed(bin_output_param);
-
+  free_if_needed(bin_input_param);                  // Free allocated memory       
+  free_if_needed(bin_output_param);                 // Free allocated memory
 
   return EXIT_SUCCESS;
 }
--- Exercice02/EX02.c
+++ Exercice02/EX02.c
@@ -1,30 +1,19 @@
-/**
- * \file skeleton.c
- * \brief Basic parsing options skeleton.
- * \author Pierre L. <pierre1.leroy@orange.com>
- * \version 0.1
- * \date 10 septembre 2016
- *
- * Basic parsing options skeleton exemple c file.
- */
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<errno.h>
-
-#include<getopt.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #define STDOUT 1
 #define STDERR 2
 
 #define MAX_PATH_LENGTH 4096
 
-
-#define USAGE_SYNTAX "[OPTIONS] -i INPUT -o OUTPUT"
+#define USAGE_SYNTAX "[OPTIONS] -i INPUT"
 #define USAGE_PARAMS "OPTIONS:\n\
   -i, --input  INPUT_FILE  : input file\n\
-  -o, --output OUTPUT_FILE : output file\n\
 ***\n\
   -v, --verbose : enable *verbose* mode\n\
   -h, --help    : display this help\n\
@@ -36,11 +25,10 @@
  *
  * \return void
  */
-void print_usage(char* bin_name)
+void print_usage(char *bin_name)
 {
   dprintf(1, "USAGE: %s %s\n\n%s\n", bin_name, USAGE_SYNTAX, USAGE_PARAMS);
 }
-
 
 /**
  * Procedure checks if variable must be free
@@ -50,11 +38,11 @@ void print_usage(char* bin_name)
  * \see man 3 free
  * \return void
  */
-void free_if_needed(void* to_free)
+void free_if_needed(void *to_free)
 {
-  if (to_free != NULL) free(to_free);  
+  if (to_free != NULL)
+    free(to_free);
 }
-
 
 /**
  *
@@ -62,22 +50,20 @@ void free_if_needed(void* to_free)
  * \see man 3 perror
  * \return
  */
-char* dup_optarg_str()
+char *dup_optarg_str()
 {
-  char* str = NULL;
+  char *str = NULL;
 
   if (optarg != NULL)
   {
     str = strndup(optarg, MAX_PATH_LENGTH);
-    
-    // Checking if ERRNO is set
-    if (str == NULL) 
+
+    if (str == NULL)
       perror(strerror(errno));
   }
 
   return str;
 }
-
 
 /**
  * Binary options declaration
@@ -86,39 +72,34 @@ char* dup_optarg_str()
  * \see man 3 getopt_long or getopt
  * \see struct option definition
  */
-static struct option binary_opts[] = 
-{
-  { "help",    no_argument,       0, 'h' },
-  { "verbose", no_argument,       0, 'v' },
-  { "input",   required_argument, 0, 'i' },
-  { "output",  required_argument, 0, 'o' },
-  { 0,         0,                 0,  0  } 
-};
+static struct option binary_opts[] =
+    {
+        {"help", no_argument, 0, 'h'},
+        {"verbose", no_argument, 0, 'v'},
+        {"input", required_argument, 0, 'i'},
+        {0, 0, 0, 0}};
 
 /**
  * Binary options string
  * (linked to optionn declaration)
  *
  * \see man 3 getopt_long or getopt
- */ 
-const char* binary_optstr = "hvi:o:";
-
-
+ */
+const char *binary_optstr = "hvi:o:";
 
 /**
  * Binary main loop
  *
- * \return 1 if it exit successfully 
+ * \return 1 if it exit successfully
  */
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   /**
    * Binary variables
    * (could be defined in a structure)
    */
   short int is_verbose_mode = 0;
-  char* bin_input_param = NULL;
-  char* bin_output_param = NULL;
+  char *bin_input_param = NULL;
 
   // Parsing options
   int opt = -1;
@@ -128,67 +109,96 @@ int main(int argc, char** argv)
   {
     switch (opt)
     {
-      case 'i':
-        //input param
-        if (optarg)
-        {
-          bin_input_param = dup_optarg_str();         
-        }
-        break;
-      case 'o':
-        //output param
-        if (optarg)
-        {
-          bin_output_param = dup_optarg_str();
-        }
-        break;
-      case 'v':
-        //verbose mode
-        is_verbose_mode = 1;
-        break;
-      case 'h':
-        print_usage(argv[0]);
-
-        free_if_needed(bin_input_param);
-        free_if_needed(bin_output_param);
- 
-        exit(EXIT_SUCCESS);
-      default :
-        break;
+    case 'i':
+      // input param
+      if (optarg)
+      {
+        bin_input_param = dup_optarg_str();
+      }
+      break;
+    case 'v':
+      // verbose mode
+      is_verbose_mode = 1;
+      break;
+    case 'h':
+      print_usage(argv[0]);
+      free_if_needed(bin_input_param);
+      exit(EXIT_SUCCESS);
+    default:
+      break;
     }
-  } 
+  }
 
   /**
    * Checking binary requirements
    * (could defined in a separate function)
    */
-  if (bin_input_param == NULL || bin_output_param == NULL)
+  if (bin_input_param == NULL)
   {
     dprintf(STDERR, "Bad usage! See HELP [--help|-h]\n");
 
     // Freeing allocated data
     free_if_needed(bin_input_param);
-    free_if_needed(bin_output_param);
     // Exiting with a failure ERROR CODE (== 1)
     exit(EXIT_FAILURE);
   }
 
-
   // Printing params
-  dprintf(1, "** PARAMS **\n%-8s: %s\n%-8s: %s\n%-8s: %d\n", 
-          "input",   bin_input_param, 
-          "output",  bin_output_param, 
+  dprintf(1, "** PARAMS **\n%-8s: %s\n%-8s: %d\n\n",
+          "input", bin_input_param,
           "verbose", is_verbose_mode);
 
   // Business logic must be implemented at this point
+  // V1.0
+  //
+  // Open the source file to read
+  int fd = open(bin_input_param, O_RDONLY);
+  if (fd == -1)
+  {
+    perror("Error opening the file");
+    free_if_needed(bin_input_param); // Free allocated memory
+    exit(EXIT_FAILURE);              // Exit the program with an error status
+  }
 
-  /* LOREM IPSUM DOT SIR AMET */
+  // Get the source file size
+  off_t file_size = lseek(fd, 0, SEEK_END);
+  if (file_size == -1)
+  {
+    perror("Error getting file size");
+    free_if_needed(bin_input_param); // Free allocated memory
+    close(fd);                       // Close the file descriptor
+    exit(EXIT_FAILURE);              // Exit the program with an error status
+  }
 
+  printf("Contents of the file %s in reverse order:\n", bin_input_param);
 
-  // Freeing allocated data
-  free_if_needed(bin_input_param);
-  free_if_needed(bin_output_param);
+  char buffer[1];                     // Allocate a buffer to store read data
+  for (off_t i = file_size - 1; i >= 0; i--)  // Loop through the file from the end to the beginning
+  {
+    // Move the file cursor to the specified position (i)
+    if (lseek(fd, i, SEEK_SET) == -1) // SEEK_SET: start of the file
+    {
+      perror("Error seeking in the file");
+      free_if_needed(bin_input_param); // Free allocated memory
+      close(fd);                       // Close the file descriptor
+      exit(EXIT_FAILURE);              // Exit the program with an error status
+    }
 
+    // Read one byte from the file into the buffer
+    if (read(fd, buffer, 1) == -1)    // Read one byte from the file into the buffer
+    {
+      perror("Error reading the file");
+      free_if_needed(bin_input_param); // Free allocated memory
+      close(fd);                       // Close the file descriptor
+      exit(EXIT_FAILURE);              // Exit the program with an error status
+    }
+
+    write(STDOUT_FILENO, buffer, 1);      // Write the byte from the buffer to the standard output
+  }
+  printf("\n");                       // Print a newline after displaying the file content
+  close(fd);                          // Close the file descriptor
+
+  free_if_needed(bin_input_param); // Free allocated memory
 
   return EXIT_SUCCESS;
 }

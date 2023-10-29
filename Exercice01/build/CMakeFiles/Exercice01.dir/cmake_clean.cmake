ee39af0a2c116fc510b5fdb9b661208cf1ea872e
AIsamet <isamet2067@gmail.com>
2023-10-29 13:54:07 +0100

    cleaned project structure

---
+++ Exercice01/build/CMakeFiles/Exercice01.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exercice01.dir/EX01.c.o"
+  "CMakeFiles/Exercice01.dir/EX01.c.o.d"
+  "Exercice01"
+  "Exercice01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Exercice01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

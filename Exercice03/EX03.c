86d89b02f1a62d4e60e4f54d0a52cffde1fb0efc
joe67100 <joefeucht67220@gmail.com>
2023-10-30 12:07:41 +0100

    feat: modified .gitignore + ex03

--- Exercice03/EX03.c
+++ Exercice03/EX03.c
@@ -1,67 +1,35 @@
-/**
- * \file skeleton.c
- * \brief Basic parsing options skeleton.
- * \author Pierre L. <pierre1.leroy@orange.com>
- * \version 0.1
- * \date 10 septembre 2016
- *
- * Basic parsing options skeleton exemple c file.
- */
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<errno.h>
-
-#include<getopt.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <getopt.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
 
 #define STDOUT 1
 #define STDERR 2
-
 #define MAX_PATH_LENGTH 4096
 
-
-#define USAGE_SYNTAX "[OPTIONS] -i INPUT -o OUTPUT"
+#define USAGE_SYNTAX "[OPTIONS] -d DIRECTORY"
 #define USAGE_PARAMS "OPTIONS:\n\
-  -i, --input  INPUT_FILE  : input file\n\
-  -o, --output OUTPUT_FILE : output file\n\
-***\n\
+  -d, --directory  DIRECTORY : directory to list\n\
   -v, --verbose : enable *verbose* mode\n\
-  -h, --help    : display this help\n\
-"
+  -h, --help    : display this help\n"
 
-/**
- * Procedure which displays binary usage
- * by printing on stdout all available options
- *
- * \return void
- */
 void print_usage(char* bin_name)
 {
   dprintf(1, "USAGE: %s %s\n\n%s\n", bin_name, USAGE_SYNTAX, USAGE_PARAMS);
 }
 
-
-/**
- * Procedure checks if variable must be free
- * (check: ptr != NULL)
- *
- * \param void* to_free pointer to an allocated mem
- * \see man 3 free
- * \return void
- */
 void free_if_needed(void* to_free)
 {
-  if (to_free != NULL) free(to_free);  
+  if (to_free != NULL) free(to_free);
 }
 
-
-/**
- *
- * \see man 3 strndup
- * \see man 3 perror
- * \return
- */
 char* dup_optarg_str()
 {
   char* str = NULL;
@@ -69,58 +37,29 @@ char* dup_optarg_str()
   if (optarg != NULL)
   {
     str = strndup(optarg, MAX_PATH_LENGTH);
-    
-    // Checking if ERRNO is set
-    if (str == NULL) 
+
+    if (str == NULL)
       perror(strerror(errno));
   }
 
   return str;
 }
 
-
-/**
- * Binary options declaration
- * (must end with {0,0,0,0})
- *
- * \see man 3 getopt_long or getopt
- * \see struct option definition
- */
-static struct option binary_opts[] = 
+static struct option binary_opts[] =
 {
   { "help",    no_argument,       0, 'h' },
   { "verbose", no_argument,       0, 'v' },
-  { "input",   required_argument, 0, 'i' },
-  { "output",  required_argument, 0, 'o' },
-  { 0,         0,                 0,  0  } 
+  { "directory", required_argument, 0, 'd' },
+  { 0,         0,                 0,  0  }
 };
 
-/**
- * Binary options string
- * (linked to optionn declaration)
- *
- * \see man 3 getopt_long or getopt
- */ 
-const char* binary_optstr = "hvi:o:";
+const char* binary_optstr = "hvd:";
 
-
-
-/**
- * Binary main loop
- *
- * \return 1 if it exit successfully 
- */
 int main(int argc, char** argv)
 {
-  /**
-   * Binary variables
-   * (could be defined in a structure)
-   */
   short int is_verbose_mode = 0;
-  char* bin_input_param = NULL;
-  char* bin_output_param = NULL;
+  char* bin_directory_param = NULL;
 
-  // Parsing options
   int opt = -1;
   int opt_idx = -1;
 
@@ -128,67 +67,76 @@ int main(int argc, char** argv)
   {
     switch (opt)
     {
-      case 'i':
-        //input param
+      case 'd':
         if (optarg)
         {
-          bin_input_param = dup_optarg_str();         
-        }
-        break;
-      case 'o':
-        //output param
-        if (optarg)
-        {
-          bin_output_param = dup_optarg_str();
+          bin_directory_param = dup_optarg_str();
         }
         break;
       case 'v':
-        //verbose mode
         is_verbose_mode = 1;
         break;
       case 'h':
         print_usage(argv[0]);
-
-        free_if_needed(bin_input_param);
-        free_if_needed(bin_output_param);
- 
+        free_if_needed(bin_directory_param);
         exit(EXIT_SUCCESS);
-      default :
+      default:
         break;
     }
-  } 
+  }
 
-  /**
-   * Checking binary requirements
-   * (could defined in a separate function)
-   */
-  if (bin_input_param == NULL || bin_output_param == NULL)
+  if (bin_directory_param == NULL)
   {
     dprintf(STDERR, "Bad usage! See HELP [--help|-h]\n");
-
-    // Freeing allocated data
-    free_if_needed(bin_input_param);
-    free_if_needed(bin_output_param);
-    // Exiting with a failure ERROR CODE (== 1)
+    free_if_needed(bin_directory_param);
     exit(EXIT_FAILURE);
   }
 
+  // List files in the specified directory
+  DIR* dir;
+  struct dirent* ent;
 
-  // Printing params
-  dprintf(1, "** PARAMS **\n%-8s: %s\n%-8s: %s\n%-8s: %d\n", 
-          "input",   bin_input_param, 
-          "output",  bin_output_param, 
-          "verbose", is_verbose_mode);
+  if ((dir = opendir(bin_directory_param)) != NULL)
+  {
+    while ((ent = readdir(dir)) != NULL)
+    {
+      char filepath[MAX_PATH_LENGTH];
+      snprintf(filepath, sizeof(filepath), "%s/%s", bin_directory_param, ent->d_name);
 
-  // Business logic must be implemented at this point
+      struct stat file_stat;
+      if (stat(filepath, &file_stat) == 0)
+      {
+        char permissions[11];
+        permissions[0] = (S_ISDIR(file_stat.st_mode)) ? 'd' : '-';
+        permissions[1] = (file_stat.st_mode & S_IRUSR) ? 'r' : '-';
+        permissions[2] = (file_stat.st_mode & S_IWUSR) ? 'w' : '-';
+        permissions[3] = (file_stat.st_mode & S_IXUSR) ? 'x' : '-';
+        permissions[4] = (file_stat.st_mode & S_IRGRP) ? 'r' : '-';
+        permissions[5] = (file_stat.st_mode & S_IWGRP) ? 'w' : '-';
+        permissions[6] = (file_stat.st_mode & S_IXGRP) ? 'x' : '-';
+        permissions[7] = (file_stat.st_mode & S_IROTH) ? 'r' : '-';
+        permissions[8] = (file_stat.st_mode & S_IWOTH) ? 'w' : '-';
+        permissions[9] = (file_stat.st_mode & S_IXOTH) ? 'x' : '-';
+        permissions[10] = '\0';
 
-  /* LOREM IPSUM DOT SIR AMET */
+        struct passwd* owner = getpwuid(file_stat.st_uid);
+        struct group* group = getgrgid(file_stat.st_gid);
 
+        char modification_time[20];
+        strftime(modification_time, sizeof(modification_time), "%y/%m/%d @ %H:%M", localtime(&file_stat.st_mtime));
 
-  // Freeing allocated data
-  free_if_needed(bin_input_param);
-  free_if_needed(bin_output_param);
+        dprintf(1, "%s %s %s %lld %s %s\n", permissions, owner->pw_name, group->gr_name, (long long)file_stat.st_size, modification_time, ent->d_name);
+      }
+    }
+    closedir(dir);
+  }
+  else
+  {
+    perror("Error opening directory");
+    free_if_needed(bin_directory_param);
+    exit(EXIT_FAILURE);
+  }
 
-
+  free_if_needed(bin_directory_param);
   return EXIT_SUCCESS;
 }
